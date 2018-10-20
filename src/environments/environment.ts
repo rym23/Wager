9d617297a5b676280df552ebb8fe3d0ab6400162
Colin Mason <masonc789@gmail.com>
2018-10-20 09:55:43 -0400

    Add socket.io dependency

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBGRKE_WHm5tmAsq1g3bMJDXAvu4Tg8EKA",
+    authDomain: "wager-a1aff.firebaseapp.com",
+    databaseURL: "https://wager-a1aff.firebaseio.com",
+    projectId: "wager-a1aff",
+    storageBucket: "",
+    messagingSenderId: "527790538791"
+  }
 };
 
 /*
