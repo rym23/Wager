bf2af3586005f11746b78093db47c8d4d03e0363
rym23 <rym23@pitt.edu>
2018-10-21 15:09:08 -0400

    update main screen to ask random questions

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  questionContent: string;
+  questionsSet: Set<string>;
 
-  constructor() { }
+  constructor() {     
+    this.questionsSet = new Set();
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "../../assets/questions.txt", false);
+    xmlhttp.send();
+    if (xmlhttp.status == 200)
+      result = xmlhttp.responseText;
+    var questionsArr = result.split("\n");
 
-  ngOnInit() {
+    for (let question of questionsArr) {
+      this.questionsSet.add(question);
+    }
+
+    this.newQuestion();
   }
 
+  ngOnInit() {
+    
+  }
+
+  newQuestion() {
+    var randIndex = Math.floor(Math.random() * (this.questionsSet.size + 1));
+    var i = 0;
+    for (var item of Array.from(this.questionsSet.values())){
+      if (i == randIndex)
+      {
+        this.questionContent = item;
+        this.questionsSet.delete(item);
+        return;
+      }
+      i++;
+    }
+  }
 }
