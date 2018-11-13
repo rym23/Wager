d5950c59181f30522cd4ad5c8a5b3ad2f5b34bb3
thartman77 <monker36@gmail.com>
2018-11-12 21:15:55 -0500

    Add firebase files

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,19 +6,3 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-
-export const deleteOldItems = functions.firestore.document('/rooms/{pushId}').onCreate((create, context) => {
-  const ref = create.ref.parent;
-  const now = Date.now();
-  const cutoff = now - 24 * 60 * 60 * 1000;
-  const oldItemsQuery = ref.orderBy('createdAt').endAt(cutoff);
-  return oldItemsQuery.get()
-  .then((snapshot) => {
-        // create a map with all children that need to be removed
-        snapshot.forEach(function(child) {
-            ref.doc(child.id).delete();
-        });
-        // execute all updates in one go and return the result to end the function
-        return ref;
-  }).catch(err => console.log(err));
-});
